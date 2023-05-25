d357dcf2fb9569c94a64d2a1b28c39018753ff3e
rashajawad <rasha.jawad2015@gmail.com>
2023-05-24 23:57:07 -0400

    new method created

---
+++ src/project1/OddOrEven.java
@@ -0,0 +1,11 @@
+package project1;
+
+public class OddOrEven {
+    public static String identify(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
